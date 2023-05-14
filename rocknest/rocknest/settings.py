ba4f01165a6da7013a062f14ce8ed0260aa19ece
Amgad <amgad.mourad18@gmail.com>
2023-05-14 18:41:06 +0300

    .

--- rocknest/rocknest/settings.py
+++ rocknest/rocknest/settings.py
@@ -107,15 +107,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# CORS_ALLOWED_ORIGINS = [
-#      "http://localhost:3000", # your frontend domain
-#      "http://127.0.0.1:3000", # your frontend domain
-#  ]
+CORS_ALLOWED_ORIGINS = [
+     "http://localhost:3000", # your frontend domain
+     "http://127.0.0.1:3000", # your frontend domain
+ ]
 
 CORS_ORIGIN_WHITELIST = [
     '*',
 ]
-CORS_ALLOWED_ORIGINS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Internationalization
