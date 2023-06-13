a44ecc6e97c501b54f520c85b21e46c89f6ec6ba
Amgad <amgad.mourad18@gmail.com>
2023-06-13 18:28:20 +0300

    postgress vercel

--- rocknest/settings.py
+++ rocknest/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#qknz7s1d&75umawu$61=zn8+v_ikbzhrd$+!r5f2xog!&-ire
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,15 +103,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'rocknest.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db.verceldb',
+        'USER' : 'default',
+        'PASSWORD' : 'ocjTVesXG5O6' ,
+        'HOST' : 'ep-autumn-sun-639850-pooler.us-east-1.postgres.vercel-storage.com'
+     
+    }
+}
 
 
 # Password validation
